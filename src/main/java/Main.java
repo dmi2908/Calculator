2b81c0c469038112719df1d97911d02a8b793497
Dmitry Borisov <dmi2908@yandex.ru>
2022-09-05 16:53:40 +0300

    output fle creation added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) throws Exception{
-        File file = new File("C:\\Users\\dmi29\\Desktop\\JavaLessons\\Calculator\\input.txt");
+        File file = new File("input.txt");
         Scanner sc = new Scanner(file);
         String str = sc.nextLine();
         sc.close();
@@ -11,32 +13,36 @@ class Main{
         double b = 0.0;
         char action = ' ';
         boolean hasError = false;
+        String result = "";
         String[] arr = str.split(" ");
         for (int i = 0; i < arr.length; i++) {
-            if(!hasError) {
-                if (i == 0) {
-                    try {
-                        a = Double.valueOf(arr[i]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Error! Not number");
-                        hasError = true;
-                    }
+            if (i == 0) {
+                try {
+                    a = Double.valueOf(arr[i]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Error! Not number");
+                    result = "Error! Not number";
+                    hasError = true;
+                    break;
                 }
-                if (i == 1) {
-                    if (arr[i].equals("/") || arr[i].equals("*") || arr[i].equals("+") || arr[i].equals("-")) {
-                        action = arr[i].charAt(0);
-                    } else {
-                        hasError = true;
-                        System.out.println("Operation Error!");
-                    }
+            }
+            if (i == 1) {
+                if (arr[i].equals("/") || arr[i].equals("*") || arr[i].equals("+") || arr[i].equals("-")) {
+                    action = arr[i].charAt(0);
+                } else {
+                    hasError = true;
+                    System.out.println("Operation Error!");
+                    result = "Operation Error!";
+                    break;
                 }
-                if (i == 2) {
-                    try {
-                        b = Double.valueOf(arr[i]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Error! Not number");
-                        hasError = true;
-                    }
+            }
+            if (i == 2) {
+                try {
+                    b = Double.valueOf(arr[i]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Error! Not number");
+                    hasError = true;
+                    break;
                 }
             }
         }
@@ -45,21 +51,33 @@ class Main{
             switch (action) {
                 case('+'):
                     System.out.println(a + b);
+                    result = String.valueOf(a + b);
                     break;
                 case('-'):
                     System.out.println(a - b);
+                    result = String.valueOf(a - b);
                     break;
                 case('/'):
                     if (b > 0.0) {
                         System.out.println(a / b);
+                        result = String.valueOf(a / b);
                     } else {
                         System.out.println("Error! Division by zero");
+                        result = "Error! Division by zero";
                     }
                     break;
                 case('*'):
                     System.out.println(a * b);
+                    result = String.valueOf(a * b);
                     break;
             }
+        }
+
+        try (FileWriter output = new FileWriter("output.txt", false)) {
+            output.write(result);
+            output.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
